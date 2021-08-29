5d9186cd91c72997897e5d9b57236473633a4cd7
Ivan Elfimov <ielfimov@gmail.com>
2021-08-29 10:04:05 +0300

    Add VIA keymap and design

---
+++ keymaps/via/README.md
@@ -0,0 +1,8 @@
+# SpiderIsland Winry25TC VIA Keymap
+
+In VIA:
+
+- open Settings and make sure "Show Design tab" is on
+- go to design tab and load `via_design.json`
+
+Now you can use Configure tab to set your keys.
---
+++ keymaps/via/rules.mk
@@ -0,0 +1,16 @@
+VIA_ENABLE = yes
+
+# Override for keeping regular build as full
+BOOTMAGIC_ENABLE = lite
+
+# For Via's compatibility
+LEADER_ENABLE = no
+FAUXCLICKY_ENABLE = no
+MIDI_ENABLE = no
+BLUETOOTH_ENABLE = no
+KEY_LOCK_ENABLE = no
+TERMINAL_ENABLE = no
+
+# Build size optimization
+LTO_ENABLE = yes
+MOUSEKEY_ENABLE = no
